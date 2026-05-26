48d0caf49ad460310dc85b4331f7c02de609e53a
epruitt <epruitt223@gmail.com>
2026-05-25 23:08:20 -0500

    added public and private route tables

--- terraform-manifest/vpc.tf
+++ terraform-manifest/vpc.tf
@@ -48,11 +48,40 @@ resource "aws_nat_gateway" "nat" {
 }
 
 #Public Route Table
+resource "aws_routee_table" "public_rt" {
+  vpc_id = aws_vpc.main.id
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+  tags = merge(var.tags,{Name = "${var.environment_name}-public-rt"})
+  
+}
 
+#Public Route Table Association to Public Subnet
+resource "aws_route_table_association" "public_rt_assoc" {
+  for_each = aws_subnet.public
+  subnet_id = each.value.id
+  route_table_id = aws_routee_table.public_rt.id
+  
+}
 
 #Private Roubte Table
-
+resource "aws_routee_table" "private_rt" {
+  vpc_id = aws_vpc.main.id
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_nat_gateway.nat.id
+  }
+  tags = merge(var.tags,{Name = "${var.environment_name}-private-rt"})
+  
+}
 
 #Private Route Table Association to Private Subnet
-
+resource "aws_route_table_association" "private_rt_assoc" {
+  for_each = aws_subnet.private
+  subnet_id = each.value.id
+  route_table_id = aws_routee_table.private_rt.id
+  
+}
 
